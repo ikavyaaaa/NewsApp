5812d7233cb8832a14c431ccfa50fa97df3205fd
Kavya <kavyakrishna301@gmail.com>
2022-08-18 19:19:34 +0530

    Coredata class

---
+++ NewsApp/Supporting Files/CoreData/News+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  News+CoreDataClass.swift
+//  
+//
+//  Created by Kavya on 18/08/22.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(News)
+public class News: NSManagedObject {
+
+}
